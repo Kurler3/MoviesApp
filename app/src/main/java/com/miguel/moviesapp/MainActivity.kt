a531f159ce5290b51cac9207b450b1d666b324f0
Kurler3 <miguelbento2000@gmail.com>
2021-03-18 10:10:57 +0800

    Added bottom nav view and fixed back button problems with the navigation component

--- app/src/main/java/com/miguel/moviesapp/MainActivity.kt
+++ app/src/main/java/com/miguel/moviesapp/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.navigation.NavController
 import androidx.navigation.NavHost
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,6 +18,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,9 +28,16 @@ class MainActivity : AppCompatActivity() {
                 .findFragmentById(R.id.nav_host_container) as NavHostFragment
         navController = navHostFragment.findNavController()
 
+        appBarConfiguration = AppBarConfiguration(
+                setOf(R.id.moviesListFragment, R.id.seriesListFragment)
+        )
+
+        setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
         bottom_nav_view.setupWithNavController(navController)
     }
 
-
-
+    override fun onSupportNavigateUp(): Boolean {
+        return super.onSupportNavigateUp() || navController.navigateUp()
+    }
 }
---
+++ app/src/main/java/com/miguel/moviesapp/ui/series/SeriesListFragment.kt
@@ -0,0 +1,7 @@
+package com.miguel.moviesapp.ui.series
+
+import androidx.fragment.app.Fragment
+import com.miguel.moviesapp.R
+
+class SeriesListFragment: Fragment(R.layout.series_list_layout) {
+}
